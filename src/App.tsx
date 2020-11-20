190536fb87bbac2da9b8b5144a4a2eb5f3811f83
Denis Uskov <udb@inbox.ru>
2020-11-20 13:06:03 +0300

    Очистка

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Добро пожаловать в React
-      </header>
+      <header className="App-header">Добро пожаловать в React</header>
     </div>
   );
 }
